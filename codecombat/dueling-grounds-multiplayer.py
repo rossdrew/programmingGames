7f941baa2096e9498b1064034e6c42f067aa2f7a
rossdrew <rosswilliamdrew@googlemail.com>
2016-05-16 09:28:42 +0100

    Some comments for clarity

--- codecombat/dueling-grounds-multiplayer.py
+++ codecombat/dueling-grounds-multiplayer.py
@@ -4,10 +4,13 @@ hidingOrSeeking = "hiding"
 def moveMe(x,y):
     hero.moveXY(x, y)
 
+#Go hide behind the traps, see if we can get them to do some work for us
 moveMe(58, 25)
 moveMe(64, 25)
 moveMe(64, 22)
 
+#  Cleave is pointless really as strongers swords (Which 
+# are needed for a decent win rate) have no cleave function
 def attack(enemy):
     if hero.isReady("cleave"):
         hero.cleave(enemy)
@@ -26,8 +29,7 @@ loop:
     else:
         waitCount += 1
         
-    
-    #Been waiting too long, go to enemy hero
+    #Been hiding too long, go to enemy hero
     if waitCount > 10:
         hero.moveXY(64, 25)
         loop:
