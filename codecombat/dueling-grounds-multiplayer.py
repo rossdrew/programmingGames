f4200b5f234c0e290cbe59d1f2fc313596419efc
Ross <thek1ng13@hotmail.com>
2016-06-21 10:47:40 +0100

    Utalizing electrocute and chain-lightning

--- codecombat/dueling-grounds-multiplayer.py
+++ codecombat/dueling-grounds-multiplayer.py
@@ -1,28 +1,29 @@
 waitCount = 0 #How long we've waited for enemy hero to come to us
 hidingOrSeeking = "hiding"
 
+hero.bash(hero.findNearestEnemy())
+hero.electrocute(hero.findNearestEnemy())
 hero.moveXY(58, 25)
 hero.moveXY(64, 25)
 hero.moveXY(64, 22)
 
 def attack(enemy):
-    if hero.isReady("cleave"):
-        hero.cleave(enemy)
-    elif hero.isReady("bash"):
-        hero.bash(enemy)
-    elif hero.distanceTo(enemyHero) < 5:
+    if hero.distanceTo(enemy) < 5:
         if hero.isReady("bash"):
-            hero.bash(enemyHero)
+            hero.bash(enemy)
+            hero.electrocute(enemy)
         else:
             hero.attack(enemy)
+    elif hero.distanceTo(enemy) < 30:
+        if hero.canCast("chain-lightning", enemy):
+            hero.cast("chain-lightning", enemy)
         
     if hidingOrSeeking == "hiding":
         hero.moveXY(63, 14)
 
 loop:
     enemyHero = hero.findNearestEnemy()    
-    #hero.electrocute(enemyHero)
-    if enemyHero != None and hero.distanceTo(enemyHero) < 9:
+    if enemyHero != None:
         attack(enemyHero)
         waitCount = 0
     else:
@@ -32,9 +33,6 @@ loop:
             hero.moveXY(64, 25)
             loop:
                 hidingOrSeeking = "seeking"
-                if hero.isReady("bash"):
-                    hero.bash(enemyHero)
-                else:
-                    hero.attack(enemy)
+                hero.attack(enemyHero)
         else:
-            hero.say("I'm waiting..." + waitCount)
+            hero.say(waitCount + " one thousand")
